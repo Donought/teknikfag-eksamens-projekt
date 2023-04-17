022e85205f7e887d9730009f9f2438f1476251ee
Donought <alfred.baly@gmail.com>
2023-04-17 14:51:05 +0200

    Made it easier to change animation

--- sketch.js
+++ sketch.js
@@ -1,40 +1,52 @@
-
-
-
-
 class Runner {
-	constructor(scale, interval) {
-		this.w = frames[0] * scale;
-		this.h = frames[0] * scale;
-		this.scale = scale;
-		this.interval = interval;
+	constructor() {
+		this.x = 0;
+		this.y = 0;
+		this.w = 32;
+		this.h = 32;
+		this.scale = 1;
+
 		this.stamp = millis();
 		this.frame = 0;
 	}
 
-	animate(x, y) {
-		if (this.stamp + this.interval < millis()) {
+	setScale(scale) {
+		this.w *= scale;
+		this.h *= scale;
+		this.scale = scale;
+	}
+
+	setPos(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	animate(frames, interval) {
+		if (this.stamp + interval < millis()) {
 			this.stamp = millis();
 			this.frame++;
 		}
-		if (4 < this.frame) {
+		if (frames.length - 1 < this.frame) {
 			this.frame = 0;
 		}
-
-		this.w = frames[this.frame].width * this.scale;
-		this.h = frames[this.frame].height * this.scale;
 
 		push();
 		imageMode(CORNER);
 		let temp = frames[this.frame];
-		image(temp, x, y, temp.width * this.scale, temp.height * this.scale);
+		image(
+			temp,
+			this.x,
+			this.y,
+			temp.width * this.scale,
+			temp.height * this.scale
+		);
 		pop();
 	}
 }
 
 let data;
 let sheet;
-let frames = [];
+let run = [];
 
 let sprite;
 
@@ -53,70 +65,82 @@ function setup() {
 		let w = children[i].getNum("w");
 		let h = children[i].getNum("h");
 
-		frames.push(sheet.get(x, y, w, h));
+		run.push(sheet.get(x, y, w, h));
 	}
 
-	sprite = new Runner(8, 100);
+	sprite = new Runner();
+	sprite.setScale(8);
+	sprite.setPos(width / 4 - sprite.w / 2, (height / 3) * 2 - sprite.h - 5);
 
-	hurdleQuestion(1, 5, "+",3);
+	hurdleQuestion(1, 5, "+", 3);
 }
 
+let lever = false;
 function draw() {
 	background(220);
 
 	push();
 	strokeWeight(10);
 	line(0, (height / 3) * 2, width, (height / 3) * 2);
-	sprite.animate(width / 4 - sprite.w / 2, (height / 3) * 2 - sprite.h - 5);
+	if (lever) {
+		sprite.animate(run, 100);
+	} else {
+		sprite.animate([run[0]], 100);
+	}
 	pop();
 }
 
-function hurdleQuestion(max, min, operator, variables) {
-  comp = [];
-  for (let i = 1; i < variables + 1; i++) {
-    comp.push(round(random(max, min)));
-  }
+function mousePressed() {
+	lever = !lever;
+	sprite.frame = 0;
+}
 
-  // Finds the answer to the question
-  ans = comp[0];
-  for (let i = 1; i < comp.length; i++) {
-    if (operator == "+") {
-      ans += comp[i];
-    }
-    if (operator == "-") {
-      ans -= comp[i];
-    }
-    if (operator == "*") {
-      ans *= comp[i];
-    }
-  }
-  // Debugging
-  /*
+function hurdleQuestion(max, min, operator, variables) {
+	comp = [];
+	for (let i = 1; i < variables + 1; i++) {
+		comp.push(round(random(max, min)));
+	}
+
+	// Finds the answer to the question
+	ans = comp[0];
+	for (let i = 1; i < comp.length; i++) {
+		if (operator == "+") {
+			ans += comp[i];
+		}
+		if (operator == "-") {
+			ans -= comp[i];
+		}
+		if (operator == "*") {
+			ans *= comp[i];
+		}
+	}
+	// Debugging
+	/*
 print("how many numbers:",comp.length)
 print("numbers are:",comp)
 print("ans is:",ans)
 */
 
-  // Defines the question as a variable so that it can be displayed.
-  // it does this by creating an array with the variables and the the operator
-  // and then turning it into a string to be displayed
+	// Defines the question as a variable so that it can be displayed.
+	// it does this by creating an array with the variables and the the operator
+	// and then turning it into a string to be displayed
 
-  txt = [];
-  for (let i = 0; i < variables; i++) {
-    txt.push(comp[i]);
-    txt.push(operator);
-  }
-  // Variable to hold the string
-  strr = "";
-  // Removes unnecessary operator
-  txt.splice(txt.length + 2, 1, "=");
+	txt = [];
+	for (let i = 0; i < variables; i++) {
+		txt.push(comp[i]);
+		txt.push(operator);
+	}
+	// Variable to hold the string
+	strr = "";
+	// Removes unnecessary operator
+	txt.splice(txt.length + 2, 1, "=");
 
-  // Places content of txt into string
-  for (let i = 0; i < txt.length; i++) {
-    strr += txt[i];
-  }
-  // Updates txt for clarity
-  txt = strr;
-  print(txt + ans);
-  return txt, ans;
+	// Places content of txt into string
+	for (let i = 0; i < txt.length; i++) {
+		strr += txt[i];
+	}
+	// Updates txt for clarity
+	txt = strr;
+	print(txt + ans);
+	return txt, ans;
 }

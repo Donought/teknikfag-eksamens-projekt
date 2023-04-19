1fc371cb8dd0943c1ad09b4a9d024364b8f2a201
Donought <alfred.baly@gmail.com>
2023-04-19 10:12:56 +0200

    Added jump mechanic and moved classes to separate files
    Frame sync between sprites is bad so we need a universal one, makes sense to add it in gameController eventually.

--- sketch.js
+++ sketch.js
@@ -1,103 +1,4 @@
-class Runner {
-	constructor() {
-		this.x = 0;
-		this.y = 0;
-		this.addY = 0;
-		this.w = 32;
-		this.h = 32;
-		this.scale = 1;
-
-		this.stamp = millis();
-		this.frame = 0;
-		this.hangFrame = 0;
-	}
-
-	setScale(scale) {
-		this.w *= scale;
-		this.h *= scale;
-		this.scale = scale;
-	}
-
-	setPos(x, y) {
-		this.x = x;
-		this.y = y;
-	}
-
-	animate(frames, interval) {
-		if (this.stamp + interval < millis()) {
-			this.stamp = millis();
-			this.frame++;
-		}
-		if (frames.length - 1 < this.frame) {
-			this.frame = 0;
-		}
-
-		this.display(frames);
-	}
-
-	display(frames) {
-		push();
-		imageMode(CORNER);
-		let temp = frames[this.frame];
-		image(
-			temp,
-			this.x,
-			this.y - this.addY,
-			temp.width * this.scale,
-			temp.height * this.scale
-		);
-		pop();
-	}
-
-	hang(altitude, interval, reps) {
-		if (this.stamp < millis()) {
-			this.stamp = millis() + interval;
-			this.hangFrame++;
-		}
-
-		this.addY =
-			altitude + altitude * cos(PI + ((2 * PI) / reps) * this.hangFrame);
-
-		this.display(animations[1]);
-	}
-}
-
-class Obstacle {
-	constructor(interval, time) {
-		this.w = 50;
-		this.h = 100;
-		this.x = (width / 3) * 5 - this.w;
-		this.y = (height / 3) * 2 - this.h;
-
-		this.interval = interval;
-		this.time = time;
-
-		this.spd = (this.interval / this.time) * (this.x - width / 3);
-
-		this.stamp = millis();
-	}
-
-	move() {
-		if (this.stamp + this.interval < millis()) {
-			this.stamp = millis();
-			this.x -= this.spd;
-		}
-	}
-
-	display() {
-		push();
-		rectMode(CORNER);
-		fill(0);
-		noStroke();
-		rect(this.x, this.y, this.w, this.h);
-		pop();
-	}
-
-	operate() {
-		this.move();
-		this.display();
-	}
-}
+let frametime = 100;
 
 let data;
 let sheet;
@@ -111,6 +12,8 @@ let wd = window.innerWidth;
 let hig = window.innerHeight;
 let guess = "";
 
+let obs;
+
 function preload() {
 	sheets.push([
 		loadXML("spritesheet/run/data.xml"),
@@ -121,8 +24,6 @@ function preload() {
 		loadImage("spritesheet/jump/sheet.png"),
 	]);
 }
-
-let obs;
 
 function setup() {
 	createCanvas(window.innerWidth, window.innerHeight);
@@ -142,10 +43,12 @@ function setup() {
 	sprite = new Runner();
 	sprite.setScale(8);
 	sprite.setPos(width / 4 - sprite.w / 2, (height / 3) * 2 - sprite.h - 5);
-  
-	obs = new Obstacle(100, 4000);
-  
+
+	obs = new Obstacle(frametime, 4000);
+
 	hurdleQuestion(1, 5, "+", 2);
+
+	//sprite.jump = !sprite.jump;
 }
 
 let lever = false;
@@ -155,25 +58,17 @@ function draw() {
 	push();
 	strokeWeight(10);
 	line(0, (height / 3) * 2, width, (height / 3) * 2);
-	if (lever) {
-		if (3 < sprite.frame) {
-			let frames = 10;
-			if (frames > sprite.hangFrame) {
-				sprite.hang(50, 100, frames);
-			} else {
-				sprite.hangFrame = 0;
-				lever = !lever;
-			}
-		} else {
-			sprite.animate(animations[1], 100);
-		}
-	} else {
-		sprite.animate(animations[0], 100);
-	}
 	pop();
-  
+
+	sprite.operate();
+
 	obs.operate();
-  
+
+	if (5 * obs.spd + width / 3 >= obs.x && !obs.jumped) {
+		sprite.jump = !sprite.jump;
+		obs.jumped = !obs.jumped;
+	}
+
 	hurdleAsk();
 }
 

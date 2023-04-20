30d0eccc8d77f68b221d9d0204c820fc9a217651
Donought <alfred.baly@gmail.com>
2023-04-20 14:20:40 +0200

    Formatting

--- sketch.js
+++ sketch.js
@@ -15,88 +15,88 @@ let guess = "";
 let obs = [];
 
 function preload() {
-  sheets.push([
-    loadXML("spritesheet/run/data.xml"),
-    loadImage("spritesheet/run/sheet.png"),
-  ]);
-  sheets.push([
-    loadXML("spritesheet/jump/data.xml"),
-    loadImage("spritesheet/jump/sheet.png"),
-  ]);
+	sheets.push([
+		loadXML("spritesheet/run/data.xml"),
+		loadImage("spritesheet/run/sheet.png"),
+	]);
+	sheets.push([
+		loadXML("spritesheet/jump/data.xml"),
+		loadImage("spritesheet/jump/sheet.png"),
+	]);
 }
 
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
+	createCanvas(window.innerWidth, window.innerHeight);
 
-  sheets.forEach((sheet, index) => {
-    let children = sheet[0].getChildren("sprite");
-    animations.push([]);
-    for (let i = 0; i < children.length; i++) {
-      let x = children[i].getNum("x");
-      let y = children[i].getNum("y");
-      let w = children[i].getNum("w");
-      let h = children[i].getNum("h");
-      animations[index].push(sheet[1].get(x, y, w, h));
-    }
-  });
+	sheets.forEach((sheet, index) => {
+		let children = sheet[0].getChildren("sprite");
+		animations.push([]);
+		for (let i = 0; i < children.length; i++) {
+			let x = children[i].getNum("x");
+			let y = children[i].getNum("y");
+			let w = children[i].getNum("w");
+			let h = children[i].getNum("h");
+			animations[index].push(sheet[1].get(x, y, w, h));
+		}
+	});
 
-  sprite = new Runner();
-  sprite.setScale(8);
-  sprite.setPos(width / 4 - sprite.w / 2, (height / 3) * 2 - sprite.h - 5);
+	sprite = new Runner();
+	sprite.setScale(8);
+	sprite.setPos(width / 4 - sprite.w / 2, (height / 3) * 2 - sprite.h - 5);
 
-  hurdleQuestion(1, 5, "+", 2);
+	hurdleQuestion(1, 5, "+", 2);
 
-  //sprite.jump = !sprite.jump;
+	//sprite.jump = !sprite.jump;
 }
 
 let stamp;
 let lever = false;
 let timerCount = 0;
 function draw() {
-  background(220);
+	background(220);
 
-  sprite.operate();
+	sprite.operate();
 
-  push();
-  strokeWeight(10);
-  line(0, (height / 3) * 2, width, (height / 3) * 2);
-  pop();
+	push();
+	strokeWeight(10);
+	line(0, (height / 3) * 2, width, (height / 3) * 2);
+	pop();
 
-  obs.forEach((val) => {
-    val.operate();
-    if (
-      (val.addTime / frametime) * val.spd + width / 4 - val.w / 2 >= val.x &&
-      !val.jumped
-    ) {
-      sprite.y = sprite.startY;
-      sprite.frame = 0;
-      sprite.hangFrame = 0;
-      sprite.stamp = millis();
-      sprite.jump = true;
-      val.jumped = true;
-    }
+	obs.forEach((val) => {
+		val.operate();
+		if (
+			(val.addTime / frametime) * val.spd + width / 4 - val.w / 2 >= val.x &&
+			!val.jumped
+		) {
+			sprite.y = sprite.startY;
+			sprite.frame = 0;
+			sprite.hangFrame = 0;
+			sprite.stamp = millis();
+			sprite.jump = true;
+			val.jumped = true;
+		}
 
-    if (!val.jumped && timerCount < 1) {
-      timerCount++;
-      push();
-      textAlign(RIGHT, TOP);
-      fill(0);
-      text("Timer: " + Math.ceil(val.timer * 10 ** -3, 0), width, 0);
-      pop();
-    }
-  });
+		if (!val.jumped && timerCount < 1) {
+			timerCount++;
+			push();
+			textAlign(RIGHT, TOP);
+			fill(0);
+			text("Timer: " + Math.ceil(val.timer * 10 ** -3, 0), width, 0);
+			pop();
+		}
+	});
 
-  if (timerCount < 1) {
-    push();
-    textAlign(RIGHT, TOP);
-    fill(0);
-    text("Timer: 0", width, 0);
-    pop();
-  }
+	if (timerCount < 1) {
+		push();
+		textAlign(RIGHT, TOP);
+		fill(0);
+		text("Timer: 0", width, 0);
+		pop();
+	}
 
-  timerCount = 0;
+	timerCount = 0;
 
-  /*push();
+	/*push();
 	strokeWeight(10);
 	line(0, (height / 3) * 2, width, (height / 3) * 2);
 	textAlign(RIGHT, TOP);
@@ -105,107 +105,107 @@ function draw() {
 	timerCount = 0;
 	pop();*/
 
-  /*if (5 * obs.spd + width / 3 >= obs.x && !obs.jumped) {
+	/*if (5 * obs.spd + width / 3 >= obs.x && !obs.jumped) {
 		sprite.frame = 0;
 		sprite.stamp = millis();
 		sprite.jump = !sprite.jump;
 		obs.jumped = !obs.jumped;
 	}*/
 
-  hurdleAsk();
+	hurdleAsk();
 }
 
 function mousePressed() {
-  obs.push(
-    new Obstacle(frametime, 4000, 400 + (sprite.totalHangFrames / 2) * 100)
-  );
+	obs.push(
+		new Obstacle(frametime, 4000, 400 + (sprite.totalHangFrames / 2) * 100)
+	);
 }
 
 function hurdleQuestion(max, min, operator, variables) {
-  comp = [];
-  for (let i = 1; i < variables + 1; i++) {
-    comp.push(round(random(max, min)));
-  }
+	comp = [];
+	for (let i = 1; i < variables + 1; i++) {
+		comp.push(round(random(max, min)));
+	}
 
-  // Finds the answer to the question
-  ans = comp[0];
-  for (let i = 1; i < comp.length; i++) {
-    if (operator == "+") {
-      ans += comp[i];
-    }
-    if (operator == "-") {
-      ans -= comp[i];
-    }
-    if (operator == "*") {
-      ans *= comp[i];
-    }
-  }
-  // Debugging
-  /*
+	// Finds the answer to the question
+	ans = comp[0];
+	for (let i = 1; i < comp.length; i++) {
+		if (operator == "+") {
+			ans += comp[i];
+		}
+		if (operator == "-") {
+			ans -= comp[i];
+		}
+		if (operator == "*") {
+			ans *= comp[i];
+		}
+	}
+	// Debugging
+	/*
 print("how many numbers:",comp.length)
 print("numbers are:",comp)
 print("ans is:",ans)
 */
 
-  // Defines the question as a variable so that it can be displayed.
-  // it does this by creating an array with the variables and the the operator
-  // and then turning it into a string to be displayed
+	// Defines the question as a variable so that it can be displayed.
+	// it does this by creating an array with the variables and the the operator
+	// and then turning it into a string to be displayed
 
-  txt = [];
-  for (let i = 0; i < variables; i++) {
-    txt.push(comp[i]);
-    txt.push(operator);
-  }
-  // Variable to hold the string
-  strr = "";
-  // Removes unnecessary operator
-  txt.splice(txt.length - 1, 1, "=");
+	txt = [];
+	for (let i = 0; i < variables; i++) {
+		txt.push(comp[i]);
+		txt.push(operator);
+	}
+	// Variable to hold the string
+	strr = "";
+	// Removes unnecessary operator
+	txt.splice(txt.length - 1, 1, "=");
 
-  // Places content of txt into string
-  for (let i = 0; i < txt.length; i++) {
-    strr += txt[i];
-  }
-  // Updates txt for clarity
-  txt = strr;
-  //print(txt + ans);
-  return txt, ans;
+	// Places content of txt into string
+	for (let i = 0; i < txt.length; i++) {
+		strr += txt[i];
+	}
+	// Updates txt for clarity
+	txt = strr;
+	//print(txt + ans);
+	return txt, ans;
 }
 
 function hurdleAsk() {
-  textAlign(CENTER, CENTER);
-  textSize(75);
-  textStyle(BOLD);
-  text(txt + guess, wd / 2, 0 + hig / 15);
-  // console.log(ans)
+	textAlign(CENTER, CENTER);
+	textSize(75);
+	textStyle(BOLD);
+	text(txt + guess, wd / 2, 0 + hig / 15);
+	// console.log(ans)
 }
 function hurdleGuess() {
-  // Adds numbers pressed to a string
-  for (let i = 0; i < 11; i++) {
-    if (key == i) {
-      guess += key;
-    }
-  }
-  // Removes last typed number if backspace is hit
-  if (keyCode == 8) {
-    if (guess.length > 0) {
-      guess = guess.substring(0, guess.length - 1);
-      //console.log("cut")
-    }
-  }
-  // Submits the guess and checks it when enter or space is hit
-  if (keyCode == 13 || keyCode == 32) {
-    if (guess == ans) {
-      hurdleQuestion(1, 5, "+", 2);
-      guess = "";
-    } else {
-      console.log("Wrong answer");
-      guess = "";
-    }
-  }
-  return guess;
+	// Adds numbers pressed to a string
+	for (let i = 0; i < 11; i++) {
+		if (key == i) {
+			guess += key;
+		}
+	}
+	// Removes last typed number if backspace is hit
+	if (keyCode == 8) {
+		if (guess.length > 0) {
+			guess = guess.substring(0, guess.length - 1);
+			//console.log("cut")
+		}
+	}
+	// Submits the guess and checks it when enter or space is hit
+	if (keyCode == 13 || keyCode == 32) {
+		if (guess == ans) {
+			hurdleQuestion(1, 5, "+", 2);
+			guess = "";
+		} else {
+			console.log("Wrong answer");
+			guess = "";
+		}
+	}
+	return guess;
 }
 
 function keyPressed() {
-  hurdleGuess();
+	hurdleGuess();
 }
 // test
--- sprites.js
+++ sprites.js
@@ -1,167 +1,167 @@
 class Runner {
-  constructor() {
-    this.x = 0;
-    this.startY = 0;
-    this.y = 0;
-    this.addY = 0;
-    this.w = 32;
-    this.h = 32;
-    this.scale = 1; // Can be changed
+	constructor() {
+		this.x = 0;
+		this.startY = 0;
+		this.y = 0;
+		this.addY = 0;
+		this.w = 32;
+		this.h = 32;
+		this.scale = 1; // Can be changed
 
-    this.stamp = millis();
-    this.frame = 0;
-    this.hangFrame = 0;
-    this.totalHangFrames = 10; // Can be changed
-    this.jump = false;
-    this.die = false;
-  }
+		this.stamp = millis();
+		this.frame = 0;
+		this.hangFrame = 0;
+		this.totalHangFrames = 10; // Can be changed
+		this.jump = false;
+		this.die = false;
+	}
 
-  setScale(scale) {
-    this.w *= scale;
-    this.h *= scale;
-    this.scale = scale;
-  }
+	setScale(scale) {
+		this.w *= scale;
+		this.h *= scale;
+		this.scale = scale;
+	}
 
-  setPos(x, y) {
-    this.x = x;
-    this.startY = y;
-    this.y = this.startY;
-  }
+	setPos(x, y) {
+		this.x = x;
+		this.startY = y;
+		this.y = this.startY;
+	}
 
-  animate(frames, interval) {
-    if (this.stamp + interval < millis()) {
-      this.stamp = millis();
-      this.frame++;
-    }
-    if (frames.length - 1 < this.frame) {
-      this.frame = 0;
-    }
+	animate(frames, interval) {
+		if (this.stamp + interval < millis()) {
+			this.stamp = millis();
+			this.frame++;
+		}
+		if (frames.length - 1 < this.frame) {
+			this.frame = 0;
+		}
 
-    this.display(frames);
-  }
+		this.display(frames);
+	}
 
-  display(frames, rotation) {
-    push();
-    let temp = frames[this.frame];
-    translate(
-      this.x + (temp.width * this.scale) / 2,
-      this.y - this.addY + (temp.height * this.scale) / 2
-    );
-    angleMode(DEGREES);
-    rotate(rotation);
-    imageMode(CENTER);
-    image(temp, 0, 0, temp.width * this.scale, temp.height * this.scale);
-    pop();
-  }
+	display(frames, rotation) {
+		push();
+		let temp = frames[this.frame];
+		translate(
+			this.x + (temp.width * this.scale) / 2,
+			this.y - this.addY + (temp.height * this.scale) / 2
+		);
+		angleMode(DEGREES);
+		rotate(rotation);
+		imageMode(CENTER);
+		image(temp, 0, 0, temp.width * this.scale, temp.height * this.scale);
+		pop();
+	}
 
-  hang(altitude, interval) {
-    if (this.stamp < millis() && this.jump) {
-      this.stamp = millis() + interval;
-      this.hangFrame++;
-    }
-    if (this.totalHangFrames < this.hangFrame) {
-      this.jump = false;
-      this.hangFrame = 0;
-    }
+	hang(altitude, interval) {
+		if (this.stamp < millis() && this.jump) {
+			this.stamp = millis() + interval;
+			this.hangFrame++;
+		}
+		if (this.totalHangFrames < this.hangFrame) {
+			this.jump = false;
+			this.hangFrame = 0;
+		}
 
-    push();
-    angleMode(RADIANS);
-    this.addY =
-      altitude +
-      altitude * cos(PI + ((2 * PI) / this.totalHangFrames) * this.hangFrame);
-    pop();
+		push();
+		angleMode(RADIANS);
+		this.addY =
+			altitude +
+			altitude * cos(PI + ((2 * PI) / this.totalHangFrames) * this.hangFrame);
+		pop();
 
-    this.display(animations[1], 0);
-  }
+		this.display(animations[1], 0);
+	}
 
-  fall(altitude, interval) {
-    if (
-      this.stamp < millis() &&
-      this.die &&
-      this.hangFrame < this.totalHangFrames
-    ) {
-      this.stamp = millis() + interval;
-      this.hangFrame++;
-    }
-    if (this.totalHangFrames < this.hangFrame) {
-      /*this.die = false;
+	fall(altitude, interval) {
+		if (
+			this.stamp < millis() &&
+			this.die &&
+			this.hangFrame < this.totalHangFrames
+		) {
+			this.stamp = millis() + interval;
+			this.hangFrame++;
+		}
+		if (this.totalHangFrames < this.hangFrame) {
+			/*this.die = false;
 			this.hangFrame = 0;*/
-    }
+		}
 
-    push();
-    angleMode(RADIANS);
-    this.addY =
-      altitude +
-      altitude * cos(PI + ((2 * PI) / this.totalHangFrames) * this.hangFrame) -
-      (100 / this.totalHangFrames) * this.hangFrame;
-    pop();
+		push();
+		angleMode(RADIANS);
+		this.addY =
+			altitude +
+			altitude * cos(PI + ((2 * PI) / this.totalHangFrames) * this.hangFrame) -
+			(100 / this.totalHangFrames) * this.hangFrame;
+		pop();
 
-    this.display(animations[1], (90 / this.totalHangFrames) * this.hangFrame);
-  }
+		this.display(animations[1], (90 / this.totalHangFrames) * this.hangFrame);
+	}
 
-  operate() {
-    if (this.jump) {
-      if (3 < sprite.frame) {
-        sprite.hang(50, frametime);
-      } else {
-        sprite.animate(animations[1], frametime);
-      }
-    } else if (this.die) {
-      if (3 < sprite.frame) {
-        sprite.fall(50, frametime);
-      } else {
-        sprite.animate(animations[1], frametime);
-      }
-    } else {
-      sprite.animate(animations[0], frametime);
-    }
-  }
+	operate() {
+		if (this.jump) {
+			if (3 < sprite.frame) {
+				sprite.hang(50, frametime);
+			} else {
+				sprite.animate(animations[1], frametime);
+			}
+		} else if (this.die) {
+			if (3 < sprite.frame) {
+				sprite.fall(50, frametime);
+			} else {
+				sprite.animate(animations[1], frametime);
+			}
+		} else {
+			sprite.animate(animations[0], frametime);
+		}
+	}
 }
 
 class Obstacle {
-  constructor(interval, time, addTime) {
-    this.w = 50;
-    this.h = 100;
-    this.x = (width / 3) * 5 - this.w;
-    this.startX = this.x;
-    this.y = (height / 3) * 2 - this.h;
+	constructor(interval, time, addTime) {
+		this.w = 50;
+		this.h = 100;
+		this.x = (width / 3) * 5 - this.w;
+		this.startX = this.x;
+		this.y = (height / 3) * 2 - this.h;
 
-    this.interval = interval;
-    this.addTime = addTime;
-    this.time = time + addTime;
+		this.interval = interval;
+		this.addTime = addTime;
+		this.time = time + addTime;
 
-    this.spd = (interval / this.time) * (this.x - width / 4);
-    //this.spd = this.x - width / 4;
+		this.spd = (interval / this.time) * (this.x - width / 4);
+		//this.spd = this.x - width / 4;
 
-    this.stamp = millis();
-    this.startStamp = millis();
+		this.stamp = millis();
+		this.startStamp = millis();
 
-    this.jumped = false;
+		this.jumped = false;
 
-    this.timer = time;
-  }
+		this.timer = time;
+	}
 
-  move() {
-    if (this.stamp + this.interval < millis()) {
-      this.stamp = millis();
-      this.x -= this.spd;
-    }
-  }
+	move() {
+		if (this.stamp + this.interval < millis()) {
+			this.stamp = millis();
+			this.x -= this.spd;
+		}
+	}
 
-  display() {
-    push();
-    rectMode(CORNER);
-    fill(0);
-    noStroke();
-    rect(this.x, this.y, this.w, this.h);
-    pop();
-  }
+	display() {
+		push();
+		rectMode(CORNER);
+		fill(0);
+		noStroke();
+		rect(this.x, this.y, this.w, this.h);
+		pop();
+	}
 
-  operate() {
-    this.move();
-    this.display();
-    this.timer =
-      this.time * ((this.x - width / 4) / (this.startX - width / 4)) -
-      this.addTime;
-  }
+	operate() {
+		this.move();
+		this.display();
+		this.timer =
+			this.time * ((this.x - width / 4) / (this.startX - width / 4)) -
+			this.addTime;
+	}
 }
